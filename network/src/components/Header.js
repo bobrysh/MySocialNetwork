efe8c23e2e793a3661fdf35c2b622b5b7442bf82
Pavel Bobryshev <bobr.8590@gmail.com>
2020-10-07 17:42:36 +0300

    skelet added

---
+++ network/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+const Header = () => {
+  return (
+      <header className={classes.header}>
+        <img alt='' src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png'></img>
+      </header>
+  );
+}
+
+export default Header;
---
+++ network/src/components/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './Profile.module.css';
+
+const Profile = () => {
+  return (
+    <div className={classes.profile}>
+    <div>
+      Img
+    </div>
+    <div>
+      Avatar + descr
+    </div>
+    <div>
+      Posts
+    </div>
+    <div>
+      new Post
+    </div>
+    <div>
+      posts
+    </div>
+  </div>
+  );
+}
+
+export default Profile;
